e66b57a2f06e285d100514f0a75f7a63e5045492
suey <yanxu@xiaoduotech.com>
2025-03-31 19:48:24 +0800

    feat: 添加了部分注释

--- src/declare.ts
+++ src/declare.ts
@@ -13,6 +13,13 @@ export interface MetadataEntries {
   'ui.layout.header.right.content': ComponentType;
   'ui.layout.header.right.after': ComponentType[];
 
+  /**
+   * rxp 垂直菜单, 位于外部, Header 左侧
+   */
   'rxp.ui.layout.vertical.nav.external': ComponentType[];
+
+  /**
+   * rxp 垂直菜单, 位于内部, Header 组件下方
+   */
   'rxp.ui.layout.vertical.nav.internal': ComponentType[];
 }
